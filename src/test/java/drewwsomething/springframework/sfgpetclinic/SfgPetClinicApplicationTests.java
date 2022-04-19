90469adf820071eb3dc7dadc0c3dd2595fedd9ce
Andrew Valle <Andrewjv@usc.edu>
2022-04-18 23:43:14 -0700

    Initial Commit

---
+++ src/test/java/drewwsomething/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package drewwsomething.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
